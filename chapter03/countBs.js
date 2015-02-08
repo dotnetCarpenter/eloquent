718e711b6be1e039228ee84e93695bdbf805e710
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-02-08 12:30:10 +0100

    Bean Counting - version 2

--- chapter03/countBs.js
+++ chapter03/countBs.js
@@ -8,9 +8,22 @@ if(process && process.argv) { // test if we are in a nodejs compatible environme
 var testString = input || "Bobby Boob Blubber";
 console.log("%s has %d capital Bs in it.", testString, countBs(testString));
 
+/*
 function countBs(string) { // version 1
   var Bs = 0;
   for(var n = 0, len = string.length; n < len; n++)
     Bs += string.charAt(n) === "B" ? 1 : 0;
   return Bs;
+}
+*/
+
+function countBs(string) { // version 2
+  return countChar(string, "B");
+}
+
+function countChar(string, char) {
+  var charsFound = 0;
+  for(var n = 0, len = string.length; n < len; n++)
+    charsFound += string.charAt(n) === char ? 1 : 0;
+  return charsFound;
 }
