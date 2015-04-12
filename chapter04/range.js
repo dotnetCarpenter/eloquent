b3e4f46541bcc0ffdaf873409432dca055f0e25e
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-04-12 16:03:58 +0200

    The Sum of a Range

---
+++ chapter04/range.js
@@ -0,0 +1,32 @@
+var input;
+if(process && process.argv) { // test if we are in a nodejs compatible environment
+  if(process.argv.length > 2) {
+    input = process.argv[process.argv.length-1];
+  }
+}
+
+var testString = input || "sum(range(1, 10))";
+console.log(testString + " eval to:", eval(testString));
+
+function sum(list) {
+  var ret = 0;
+  for(var i = 0, len = list.length; i < len; i++) {
+    ret += list[i];
+  }
+  return ret;
+}
+
+function range(start, end, step) {
+  var ret = [];
+  var resolve = step < 0 ?
+                  function(s, e) { return s >= e; } :
+                  function(s, e) { return s <= e; };
+  step = step || 1;
+
+  while( resolve(start, end) ) {
+    ret.push(start);
+    start += step
+  }
+
+  return ret;
+};
