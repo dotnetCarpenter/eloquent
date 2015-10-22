9c93f6aa4017cb6c3b99abe868eb6d578baf7c51
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-10-22 16:43:01 +0200

    [recusive] range and sum function

--- chapter04/recursiveRange.js
+++ chapter04/recursiveRange.js
@@ -11,10 +11,14 @@ var testString = input || "sum(range(1, 10))"
 console.log(testString + " eval to:", eval(testString))
 
 function sum(array) {
-
+  if(array.length === 0) return 0
+  let x = array[0]
+  return x + sum(array.slice(1))
 }
 
 function range(start, end, step) {
-  if(end === 0) return []
-  return [start].concat( range(start+1, end-1) )
+  step = step || 1
+  if(step > 0 && start > end) return []
+  if(step < 0 && start < end) return []
+  return [start].concat( range(start+step, end, step) )
 }
