7f268bda4777c2e49a13d45e6c9379f1c4306447
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-10-22 20:26:31 +0200

    [exercise] A List

--- chapter04/lists.js
+++ chapter04/lists.js
@@ -1,44 +1,67 @@
-"use strict";
+"use strict"
 
-var util = require("../util.js");
-
-util.addTest("arrayToList([1,2,3])");
-util.addTest("listToArray(arrayToList([1,2,3]))");
-var code = util.getTestCode();
-try {
-  console.log(code + " eval to:", eval(code));
-} catch(err) {
-  console.log("Error in js", err);
+var input;
+if(process && process.argv) { // test if we are in a nodejs compatible environment
+  if(process.argv.length > 2) {
+    input = process.argv[process.argv.length-1];
+  }
 }
 
-function arrayToList(array, list) {
-  var newList = {
-    value: array.shift(),
-    rest: list || null
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest : {
+      value: 3,
+      rest: null
+    }
   }
-  return array.length === 0 ?
-    newList :
-    arrayToList(array, newList);
+}
+var array = [1,2,3]
+
+var testString = input || "equal(list, arrayToList(array) )";
+console.log(testString + " eval to:", eval(testString));
+
+/*
+console.log(arrayToList([10, 20]));
+console.log("{ value: 10, rest: { value: 20, rest: null } }")
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log("[ 10, 20, 30 ]")
+console.log(prepend(10, prepend(20, null)));
+console.log("{ value: 10, rest: { value: 20, rest: null } }")
+console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log("20")
+*/
+
+function equal(a, b) {
+  console.dir(a)
+  console.dir(b)
+  return "Can you spot the difference?"
+}
+
+function arrayToList(array) {
+  let list = null
+  for (let i = array.length-1; i >= 0 ;--i)
+    list = {value: array[i], rest: list}
+  return list
 }
 
 function listToArray(list) {
-  var ret = [];
-  depthFirst(list, ret);
-  return ret;
+  if(!list.rest) return list.value
+  let x = list.value,
+      xs= list.rest
+  return [x].concat(listToArray(xs))
 }
-function depthFirst(list, output) {
-  for(var prop in list) {
-    util.log(prop, list[prop], list);
-    if(typeof prop === "object")
-       return depthFirst(prop);
-    output.push(list[prop])
+
+function prepend(element, list) {
+  return {
+    value: element,
+    rest: list
   }
 }
 
-function prepend(n, list) {
-
-}
-
-function nth(list, n) {
-
+function nth(list, index) {
+  if(!list) return undefined
+  if(index === 0) return list.value
+  return nth(list.rest, --index)
 }
