aa544db74b732d3b8928a62bb28b03a719515e6d
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-01 14:55:36 +0100

    apparently semicolon (;) matters

--- chapter05/flatten.js
+++ chapter05/flatten.js
@@ -1,15 +1,15 @@
 "use strict"
 
-module.exports = flatten
+module.exports = flatten;
 
 // flatten :: [a] -> [a]
 function flatten(array) {
 	return array.reduce( (b,c) => {
 		let bIsArray = Array.isArray(b),
-				cIsArray = Array.isArray(c)
+				cIsArray = Array.isArray(c);
 		return (bIsArray && cIsArray) ? flatten(b.concat(c)) :
 					(bIsArray && !cIsArray) ? b.concat([c]) :
 					(!bIsArray && cIsArray) ? [b].concat(c) :
-																		[b].concat([c])
+																		[b].concat([c]);
 	})
 }
