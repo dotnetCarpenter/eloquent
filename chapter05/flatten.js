1e42e0f5586d49ae4877280d0da46d18f7402e9c
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-01 15:57:49 +0100

    tail call does not help

--- chapter05/flatten.js
+++ chapter05/flatten.js
@@ -7,9 +7,28 @@ function flatten(array) {
 	return array.reduce( (b,c) => {
 		let bIsArray = Array.isArray(b),
 				cIsArray = Array.isArray(c);
-		return (bIsArray && cIsArray) ? flatten(b.concat(c)) :
-					(bIsArray && !cIsArray) ? b.concat([c]) :
-					(!bIsArray && cIsArray) ? [b].concat(c) :
-																		[b].concat([c]);
+		const actions = [concat, append, prepend, add];
+		let action;
+		if(bIsArray && cIsArray) action = actions[0];
+		if(bIsArray && !cIsArray) action = actions[1];
+		if(!bIsArray && cIsArray) action = actions[2];
+		if(!bIsArray && !cIsArray) action = actions[3]
+		return action(b,c);
 	})
+	
+	function append(list, value) {
+		list.push(value);
+		return list;
+	}
+	function prepend(value, list) {
+		list.unshift(value);
+		return list;
+	}
+	function concat(list1, list2) {
+		let list = list1.concat(list2);
+		return flatten(list);
+	}
+	function add(a,b) {
+		return [a,b];
+	}
 }
