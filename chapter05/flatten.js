2848952c2a31ed5ccc40ae0c9388fedec14d3503
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-01 10:26:53 +0100

    first try with flatten

---
+++ chapter05/flatten.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const dimensional = [[1,2,3], ["a", "b", "c"]]
+const crazy = [1,2,[3],["a",["b","c"]],["Eloquent"],["JavaScript"]]
+var l = console.log
+function flatten(array) {
+	return array.reduce( (b,c) => {
+		let bIsArray = Array.isArray(b),
+				cIsArray = Array.isArray(c)
+		return (bIsArray && cIsArray) ? (l("both are arrays"),b.concat(c)) :
+					(!bIsArray && cIsArray) ? (l("c is array"),[b].concat(c)) :
+					(bIsArray && !cIsArray) ? (l("b is array"),b.concat([c])) :
+																		(l("none is array"),[b].concat([c]))  
+	})
+}
+console.log(crazy)
+console.log( flatten(crazy) )
