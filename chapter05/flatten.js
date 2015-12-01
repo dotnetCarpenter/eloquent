a6f0c07769181ca772a0c5bd587357e2319a06b5
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-01 16:10:01 +0100

    moving function declarations out of the flatten function helps a lot but it is still slower than the originale

--- chapter05/flatten.js
+++ chapter05/flatten.js
@@ -4,31 +4,33 @@ module.exports = flatten;
 
 // flatten :: [a] -> [a]
 function flatten(array) {
-	return array.reduce( (b,c) => {
-		let bIsArray = Array.isArray(b),
-				cIsArray = Array.isArray(c);
-		const actions = [concat, append, prepend, add];
-		let action;
-		if(bIsArray && cIsArray) action = actions[0];
-		if(bIsArray && !cIsArray) action = actions[1];
-		if(!bIsArray && cIsArray) action = actions[2];
-		if(!bIsArray && !cIsArray) action = actions[3]
-		return action(b,c);
-	})
+	return array.reduce(flat)
+}
+
+function flat(b,c) {
+	let bIsArray = Array.isArray(b),
+			cIsArray = Array.isArray(c);
+	const actions = [concat, append, prepend, add];
+	let action;
+	if(bIsArray && cIsArray) action = actions[0];
+	if(bIsArray && !cIsArray) action = actions[1];
+	if(!bIsArray && cIsArray) action = actions[2];
+	if(!bIsArray && !cIsArray) action = actions[3]
+	return action(b,c);
+}
 	
-	function append(list, value) {
-		list.push(value);
-		return list;
-	}
-	function prepend(value, list) {
-		list.unshift(value);
-		return list;
-	}
-	function concat(list1, list2) {
-		let list = list1.concat(list2);
-		return flatten(list);
-	}
-	function add(a,b) {
-		return [a,b];
-	}
+function append(list, value) {
+	list.push(value);
+	return list;
+}
+function prepend(value, list) {
+	list.unshift(value);
+	return list;
+}
+function concat(list1, list2) {
+	let list = list1.concat(list2);
+	return flatten(list);
+}
+function add(a,b) {
+	return [a,b];
 }
