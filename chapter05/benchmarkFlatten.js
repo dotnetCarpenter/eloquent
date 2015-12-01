c022a9f9757be72065c0cb21d084913be95aab8f
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-01 13:23:12 +0100

    bench mark test

---
+++ chapter05/benchmarkFlatten.js
@@ -0,0 +1,64 @@
+"use strict"
+
+const flatten = require("./flatten")
+const dimensional = [[1,2,3], ["a", "b", "c"]]
+const wild = ["a",["b"],"c",1,2,3]
+const crazy = [1,[2],[],3,["a",["b","c"]],["Eloquent"],["JavaScript"]]
+
+let l = console.log
+outputBenchMark(1)
+outputBenchMark(2)
+outputBenchMark(4)
+outputBenchMark(1e4)
+outputBenchMark(2e4)
+outputBenchMark(3e4)
+outputBenchMark(4e4)
+outputBenchMark(8e4)
+outputBenchMark(1e5)
+
+function outputBenchMark(bennchmarkRuns) {
+	//l(crazy)
+	const result = doBenchMark(crazy, bennchmarkRuns)
+	//l( result.flatArray )
+	if(!result.passed) {
+		l("flatten didn't flat the array")
+		return
+	}
+	l("Number of Runs\tTotal Time\t1 Pass Time\t\tMean Time")
+	l("%d\t\t%s\t%s\t\t%s", result.testRuns, result.duration + "s", (result.randomDuration[0] * 1e9 + result.randomDuration[1])/1e9 + "s", result.meanDuration + "s")
+}
+
+function doBenchMark(testcase, howmany) {
+	let times = new Array(howmany)
+	let c = 0
+	let flatArray
+	
+	do {
+		let start = process.hrtime()
+		flatArray = flatten(crazy)
+		times[c] = process.hrtime(start)
+		c++
+	} while(c < howmany)
+	
+	let aggregated = times.reduce((a,b) => zipWith(add, a, b))
+	let seconds = (aggregated[0] * 1e9 + aggregated[1]) / 1e9
+
+	return {
+		flatArray,
+		testRuns: howmany,
+		duration: seconds,
+		randomDuration: times[ Math.floor(Math.random()*howmany) ],
+		meanDuration: seconds/howmany,
+		passed: flatArray.every(x => !Array.isArray(x))
+	}
+}
+
+function zipWith(f, xs, ys) {
+  if(xs.length === 0) return []
+  if(ys.length === 0) return []
+  let x = xs[0]
+  let y = ys[0]
+  return [f(x, y)].concat(zipWith(f, xs.slice(1), ys.slice(1)))
+}
+
+function add(a, b) { return a+b }
--- chapter05/flatten.js
+++ chapter05/flatten.js
@@ -1,22 +1,15 @@
 "use strict"
 
-const dimensional = [[1,2,3], ["a", "b", "c"]]
-const wild = ["a",["b"],"c",1,2,3]
-const crazy = [1,[2],[],3,["a",["b","c"]],["Eloquent"],["JavaScript"]]
-var l = console.log
+module.exports = flatten
 
-// flatten :: [a]
+// flatten :: [a] -> [a]
 function flatten(array) {
 	return array.reduce( (b,c) => {
 		let bIsArray = Array.isArray(b),
 				cIsArray = Array.isArray(c)
-		return (bIsArray && cIsArray) ? (l("both are arrays"),flatten(b.concat(c))) :
-					(bIsArray && !cIsArray) ? (l("b is array"),b.concat([c])) :
-					(!bIsArray && cIsArray) ? (l("c is array"),[b].concat(c)) :
-																		(l("none is array"),[b].concat([c]))  
+		return (bIsArray && cIsArray) ? flatten(b.concat(c)) :
+					(bIsArray && !cIsArray) ? b.concat([c]) :
+					(!bIsArray && cIsArray) ? [b].concat(c) :
+																		[b].concat([c])
 	})
 }
-l(crazy)
-let flatArray = flatten(crazy)
-l( flatArray )
-l( "Test case pass:", flatArray.every(x => !Array.isArray(x)) )
