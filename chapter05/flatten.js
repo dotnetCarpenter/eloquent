db049915239f6b4f29dfc9db453a974040328625
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-01 10:40:30 +0100

    third test case - test still pass

--- chapter05/flatten.js
+++ chapter05/flatten.js
@@ -2,16 +2,21 @@
 
 const dimensional = [[1,2,3], ["a", "b", "c"]]
 const crazy = [1,2,[3],["a",["b","c"]],["Eloquent"],["JavaScript"]]
+const wild = ["a",["b"],"c",1,2,3]
 var l = console.log
+
+// flatten :: [a]
 function flatten(array) {
 	return array.reduce( (b,c) => {
 		let bIsArray = Array.isArray(b),
 				cIsArray = Array.isArray(c)
 		return (bIsArray && cIsArray) ? (l("both are arrays"),flatten(b.concat(c))) :
-					(!bIsArray && cIsArray) ? (l("c is array"),[b].concat(c)) :
 					(bIsArray && !cIsArray) ? (l("b is array"),b.concat([c])) :
+					(!bIsArray && cIsArray) ? (l("c is array"),[b].concat(c)) :
 																		(l("none is array"),[b].concat([c]))  
 	})
 }
-console.log(crazy)
-console.log( flatten(crazy) )
+l(wild)
+let flatArray = flatten(wild)
+l( flatArray )
+l( "Test case pass:", flatArray.every(x => !Array.isArray(x)) )
