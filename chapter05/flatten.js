5b30a5e2b2b319c7299253a9ed3bfea282ae7bde
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-03 02:31:16 +0100

    [optimize] turns out that changing the anonymous function to a named function outside flatten and make both functions mutual recursive is MAJOR speedup

--- chapter05/flatten.js
+++ chapter05/flatten.js
@@ -4,13 +4,15 @@ module.exports = flatten;
 
 // flatten :: [a] -> [a]
 function flatten(array) {
-	return array.reduce( (b,c) => {
-		let bIsArray = Array.isArray(b),
-				cIsArray = Array.isArray(c);
-		
-		return (!bIsArray && !cIsArray) ? [b,c] :
-						(bIsArray && !cIsArray) ? b.concat([c]) :
-						(!bIsArray && cIsArray) ? [b].concat(c) :
-																			flatten(b.concat(c));
-	})
+	return array.reduce(reduceFlat);
+}
+
+function reduceFlat(a,b) {
+	let aIsArray = Array.isArray(a),
+			bIsArray = Array.isArray(b);
+
+	return (!aIsArray && !bIsArray) ? [a,b] :
+					(aIsArray && !bIsArray) ? a.concat([b]) :
+					(!aIsArray && bIsArray) ? [a].concat(b) :
+																		flatten(a.concat(b));
 }
