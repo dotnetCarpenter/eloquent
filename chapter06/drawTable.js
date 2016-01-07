14f69fb16e4005b00fe23308018fcb806fd77784
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-01-07 11:03:31 +0200

    Laying out a table

--- chapter06/drawTable.js
+++ chapter06/drawTable.js
@@ -34,3 +34,79 @@ function drawTable(rows) {
 
   return rows.map(drawRow).join('\n');
 }
+
+function replicate(string, times) {
+  return times < 1 ? '' : (string + replicate(string, times - 1));
+}
+
+function TextCell(text) {
+  this.text = text.split('\n');
+}
+TextCell.prototype.minWidth = function() {
+  return this.text.reduce((width, line) =>
+    Math.max(width, line.length), 0);
+};
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+  let result = [];
+  for (let i = 0; i < height; i++) {
+    let line = this.text[i] || '';
+    result.push(line + replicate(' ', width - line.length));
+  }
+  return result;
+};
+
+function UnderlinedCell(inner) {
+  this.inner = inner;
+}
+UnderlinedCell.prototype.minWidth = function() {
+  return this.inner.minWidth();
+};
+UnderlinedCell.prototype.minHeight = function() {
+  return this.inner.minHeight() + 1;
+};
+UnderlinedCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height - 1)
+    .concat([replicate('-', width)]);
+};
+
+function RTextCell(text) {
+  TextCell.call(this, text);
+}
+RTextCell.prototype = Object.create(TextCell.prototype);
+RTextCell.prototype.draw = function(width, height) {
+  let result = [];
+  for(let i = 0; i < height; i++) {
+    let line = this.text[i] || '';
+    result.push(replicate(' ', width - line.length) + line);
+  }
+  return result;
+};
+
+// TEST
+function dataTable(data) {
+  let keys = Object.keys(data[0]);
+  let headers = keys.map(name => new UnderlinedCell(new TextCell(name)));
+  let body = data.map(row =>
+    keys.map(name => {
+      let value = row[name];
+      if(typeof value == 'number')
+        return new RTextCell(String(value));
+      else
+        return new TextCell(String(value));
+    })
+  );
+  return [headers].concat(body);
+}
+
+let datalist = [
+  {name: "Anita", "super power": "Fearful wrath", team: "ISKRA"},
+  {name: "Iskra", "super power": "Sweet peeps", team: "ISKRA"},
+  {name: "Jon", "super power": "Dodged smiles", team: "ISKRA"},
+];
+// let datalist = require('./mountains');
+console.log(
+  drawTable(dataTable(datalist))
+);
