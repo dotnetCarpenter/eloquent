06f2d9485ec9717074ff6318c16121a6635a692d
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-11-22 01:37:34 +0100

    halfway through the project

--- ch07/life.js
+++ ch07/life.js
@@ -1,26 +1,4 @@
-const plan = ["############################",
-							"#                       ##o#",
-							"#           o          #   #",
-							"#               ###        #",
-							"#   ###           #        #",
-							"#    #    o        #       #",
-							"#    #                     #",
-							"#          #               #",
-							"#                          #",
-							"#      #           ### #   #",
-							"#     o               oo   #",
-							"############################"]
-
-const directions = {
-	n:  new Vector( 0, -1),
-	ne: new Vector( 1, -1),
-	e:  new Vector( 1,  0),
-	se: new Vector( 1,  1),
-	s:  new Vector( 0,  1),
-	sw: new Vector(-1,  1),
-	w:  new Vector(-1,  0),
-	nw: new Vector(-1, -1)
-}
+"use strict"
 
 class Vector {
 	constructor(x, y) {
@@ -52,9 +30,114 @@ class Grid {
 	set(vector, value) {
 		this.space[vector.x + this.width * vector.y] = value
 	}
+
+  forEach(f, context) {
+    for(let y = 0; y < this.height; y++) {
+      for(let x = 0; x < this.width; x++) {
+        const value = this.space[x + y * this.width]
+       // if(value != null)
+          f.call(context, value, new Vector(x, y))
+      }
+    }
+  }
 }
+
+class World {
+  constructor(map, legend) {
+    this.grid = new Grid(map[0].length, map.length)
+    this.legend = legend
+
+    map.forEach( (line, y) => {
+      for (let x = 0; x < line.length; x++)
+        this.grid.set( new Vector(x, y),
+                  elementFromChar(legend, line[x]) )
+    })
+  }
+
+  toString() {
+    let output = ""
+    let row = 0
+    this.grid.forEach((val, vector) => {
+      if(row < vector.y) row++, output += "\n"
+      output += val == null ? " " : val.originChar
+    })
+    /*for(let y = 0; y < this.grid.height; y++) {
+      for(let x = 0; x < this.grid.width; x++) {
+        const element = this.grid.get(new Vector(x, y))
+        output += charFromElement(element)
+      }
+      output += "\n"
+    }*/
+    return output
+  }
+
+}
+
+function Wall() {}
+
+class BouncingCritter {
+  constructor() {
+    this.direction = randomElement(directionNames)
+  }
+
+  act(view) {
+    if (view.look(this.direction) != " ")
+      this.direction = view.find( ) || "s"
+    return {type: "move", direction: this.direction}
+  }
+}
+
+
+const plan = ["############################",
+              "#                       ##o#",
+              "#           o          #   #",
+              "#               ###        #",
+              "#   ###           #        #",
+              "#    #    o        #       #",
+              "#    #                     #",
+              "#          #               #",
+              "#                          #",
+              "#      #           ### #   #",
+              "#     o               oo   #",
+              "############################"]
+
+const directions = {
+	n:  new Vector( 0, -1),
+	ne: new Vector( 1, -1),
+	e:  new Vector( 1,  0),
+	se: new Vector( 1,  1),
+	s:  new Vector( 0,  1),
+	sw: new Vector(-1,  1),
+	w:  new Vector(-1,  0),
+	nw: new Vector(-1, -1)
+}
+
+const directionNames = Object.keys(directions)
+
+// utils
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function elementFromChar(legend, ch) {
+  if(ch == " ") return null
+
+  const element = new legend[ch]
+  element.originChar = ch
+  return element
+}
+
+function charFromElement(element) {
+  if(!element) return " "
+  return element.originChar
+}
+
 
 module.exports = {
 	Grid,
-	Vector
+	Vector,
+  World,
+  Wall,
+  BouncingCritter,
+  directionNames
 }
--- ch07/test.js
+++ ch07/test.js
@@ -1,15 +1,43 @@
+"use strict"
+
 const assert = require("assert")
 const life = require("./life")
 const Grid = life.Grid
 const Vector = life.Vector
+const directionNames = life.directionNames
+const World = life.World
+const Wall = life.Wall
+const BouncingCritter = life.BouncingCritter
+
 
 assert.doesNotThrow(() => new Grid(5, 5))
 
-const grid = new Grid(5, 5)
+let grid = new Grid(5, 5)
 assert.equal(grid.get(new Vector(1, 1)), undefined)
 
 grid.set(new Vector(1, 1), "X")
 assert.equal(grid.get(new Vector(1, 1)), "X")
+
+assert.deepEqual(directionNames.join(" "), "n ne e se s sw w nw")
+
+
+const plan = ["############################",
+              "#                       ##o#",
+              "#           o          #   #",
+              "#               ###        #",
+              "#   ###           #        #",
+              "#    #    o        #       #",
+              "#    #                     #",
+              "#          #               #",
+              "#                          #",
+              "#      #           ### #   #",
+              "#     o               oo   #",
+              "############################"]
+let world = new World(plan, { "#": Wall, o: BouncingCritter})
+let result = world.toString()
+//assert.equal(plan.join("\n") + "\n", result, "Its equal to 'plan' but with a new-line in the end")
+assert.equal(plan.join("\n"), result)
+
 
 /*console.dir(assert)*/
 console.log("Everything looks a'ok")
