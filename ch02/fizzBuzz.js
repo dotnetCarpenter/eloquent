cfe0f33e6d8b0ca837d587d70962e24425d653c4
dotnetCarpenter <jon.ronnenberg@gmail.com>
2020-06-23 07:18:56 +0200

    [FizzBuzz] third version (obvious, I didn't understand the task before)

--- ch02/fizzBuzz.js
+++ ch02/fizzBuzz.js
@@ -1,14 +1,15 @@
-// first version
-/*
+'use strict'
+
 for (var c = 1; c <= 100; c++)
-  console.log(
+/*
+// first version
+console.log(
     c % 3 === 0 ?
-      "Fizz" :
+    "Fizz" :
       c % 5 === 0 ?
-        "Buzz" :
-        c
-  )
-*/
+      "Buzz" :
+      c
+      )
 // second version
 for (var c = 1; c <= 100; c++)
   console.log(
@@ -16,3 +17,16 @@ for (var c = 1; c <= 100; c++)
       "FizzBuzz" :
       c
   )
+*/
+// third version (obvious, I didn't understand the task before)
+  console.log(fizzbuzz(c))
+
+function fizzbuzz (n) {
+  let s = ''
+
+  if (n % 3 === 0) s = "Fizz"
+  if (n % 5 === 0) s += "Buzz"
+  if (s.length === 0) s = n
+
+  return s
+}
