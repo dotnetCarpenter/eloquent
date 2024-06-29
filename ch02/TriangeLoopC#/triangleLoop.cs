a02a3af12ac9d373084cbe87e023c4eb8d4a52a9
dotnetCarpenter <jon.ronnenberg@gmail.com>
2024-06-29 03:29:18 +0200

    [TriangleLoop] C# version

---
+++ ch02/TriangeLoopC#/triangleLoop.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace ch02
+{
+    class Triangle
+    {
+        static string Loop (int n)
+        {
+            var s = new StringBuilder ();
+            var sq = "";
+
+            for (var c = 0; c < n; c++)
+            {
+                sq += '#';
+                s.Append (sq);
+                s.Append ('\n');
+            }
+
+            return s.ToString ();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine (Loop (7));
+        }
+    }
+}
--- ch02/triangleLoop.js
+++ ch02/triangleLoop.js
@@ -1,2 +1,4 @@
+'use strict'
+
 for (var c = 0, sq = "#"; c < 7; c++, sq += "#")
-  console.log( sq )
+  console.log (sq)
